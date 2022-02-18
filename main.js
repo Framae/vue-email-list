a1de7b05ecd1672d66941fa8b87fb37adcca9b58
Francesco Maestrelli <framae26@gmail.com>
2022-02-18 19:32:46 +0100

    this.emails.push

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const app = new Vue (
     {
-        
+        el:'#root',
+        data: {
+            emails: [],
+        },
+        mounted() {
+            for (let i=0; i<=9; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((result) => {
+                    this.emails.push(result.data.response);
+                }
+                
+                );
+            }
+        }
     }
-)
+);
